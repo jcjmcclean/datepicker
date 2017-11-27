e86859bf4b9cb63c7334ca788f9f58ecd7cde1f6
James McClean <jcjmcclean@gmail.com>
2017-11-27 13:12:59 +0000

    Changed template to use ''+ instead of backticks

--- datepicker.directive.js
+++ datepicker.directive.js
@@ -16,29 +16,28 @@ function datePicker() {
 			maxView: "@"
 		},
 		link: link,
-		template: `
-			<span
-					moment-picker="ngModel"
-					locale="en-gb"
-					min-view="year"
-					max-view="{{ maxView || 'month' }}"
-					today="true"
-					start-view="year"
-					class="moment-picker-input"
-					format="{{ momentFormat || 'L' }}"
-					autoclose="true"
-					min-date="minDate"
-			>
-				<input
-						readonly
-						type="text"
-						ng-model="ngModel"
-						placeholder="{{ placeholder }}"
-						ng-required="required"
-						ng-model-options="{ updateOn: 'blur' }"
-				>
-			</span>
-		`
+		template: ''+
+			'<span'+
+				'moment-picker="ngModel"'+
+				'locale="en-gb"'+
+				'min-view="year"'+
+				'max-view="{{ maxView || \'month\' }}"'+
+				'today="true"'+
+				'start-view="year"'+
+				'class="moment-picker-input"'+
+				'format="{{ momentFormat || \'L\' }}"'+
+				'autoclose="true"'+
+				'min-date="minDate"'+
+			'>'+
+				'<input'+
+					'readonly'+
+					'type="text"'+
+					'ng-model="ngModel"'+
+					'placeholder="{{ placeholder }}"'+
+					'ng-required="required"'+
+					'ng-model-options="{ updateOn: \'blur\' }"'+
+				'>'+
+			'</span>'
 	};
 
 	return directive;
